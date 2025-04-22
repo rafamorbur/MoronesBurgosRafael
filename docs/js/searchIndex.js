eaa424fcf6d25bb0a17fe385aa31fd608fd8ead9
Rafael Morones Burgos <rafael.morones@gmail.com>
2025-04-22 19:58:48 +0200

    Primera subida.proyecto DAW05.practica-morones.php

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\multiplicar\u0028\u0029",
+            "name": "multiplicar",
+            "summary": "C\u00E1lculo\u0020del\u0020producto\u0020de\u0020dos\u0020n\u00FAmeros\u0020enteros",
+            "url": "namespaces/default.html#function_multiplicar"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
---
+++ practica-morones.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* Script para la tarea 05 DAW documentación código fuente PHP
+*
+*@version 1.0
+*@author Rafael Morones Burgos
+*@package tarea 05
+*@since 2025
+*/
+
+/**
+*Cálculo del producto de dos números enteros
+*
+*Esta función recibe dos números enteros y devuelve el producto
+*
+*@param int $num1 El primer número entero.
+*@param int $num2 El segundo número entero.
+*@return int El producto de los dos números enteros
+*/
+
+function multiplicar($num1, $num2 ){
+    return $num1*$num2;
+}
+
+/**
+* Cálculo de la suma de dos números enteros
+*
+* Esta función recibe dos números enteros y devuelve la suma
+*
+* @param int $num1 El primer número entero.
+* @param int $num2 El segundo número entero.
+* @return int La suma de los dos números enteros
+* @internal Comentario solo para desarrolladores. Esta función es de uso interno para la *operación de suma. Fuera de aquí no se recomienda su uso
+*/
+function sumar($num1, $num2){
+    return $num1 + $num2;
+}
+?>
